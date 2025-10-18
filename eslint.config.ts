dfee965943231a28f0096c1b56c5d0ee4f32b99a
Cade <foundernq@gmail.com>
2025-10-18 15:17:49 +1000

    fix: updated to use eslint config ts

--- eslint.config.ts
+++ eslint.config.ts
@@ -1,10 +1,63 @@
 import { defineConfig, globalIgnores } from 'eslint/config';
 import tseslint from 'typescript-eslint';
+import nx from '@nx/eslint-plugin';
 
 export default defineConfig([
-    globalIgnores([ 'dist', 'reports', 'coverage' ]),
-    tseslint.configs.recommended,
-    {
-        files: ['./src/*.ts']
-    }
+  globalIgnores(['dist', 'reports', 'coverage', 'angular-microsite']),
+  tseslint.configs.recommended,
+  nx.configs['flat/base'],
+  nx.configs['flat/typescript'],
+  nx.configs['flat/javascript'],
+  {
+    files: ['./src/*.ts'],
+  },
+  {
+    files: ['**/*.json'],
+    // Override or add rules here
+    rules: {},
+    languageOptions: {
+      parser: await import('jsonc-eslint-parser'),
+    },
+  },
+  {
+    files: ['**/*.ts', '**/*.tsx', '**/*.js', '**/*.jsx'],
+    rules: {
+      '@nx/enforce-module-boundaries': [
+        'error',
+        {
+          enforceBuildableLibDependency: true,
+          allow: ['^.*/eslint(\\.base)?\\.config\\.[cm]?[jt]s$'],
+          depConstraints: [
+            {
+              sourceTag: '*',
+              onlyDependOnLibsWithTags: ['*'],
+            },
+          ],
+        },
+      ],
+    },
+  },
+  {
+    files: [
+      '**/*.ts',
+      '**/*.tsx',
+      '**/*.cts',
+      '**/*.mts',
+      '**/*.js',
+      '**/*.jsx',
+      '**/*.cjs',
+      '**/*.mjs',
+    ],
+    // Override or add rules here
+    rules: {},
+  },
+  {
+    files: ['**/package.json', '**/generators.json'],
+    rules: {
+      '@nx/nx-plugin-checks': 'error',
+    },
+    languageOptions: {
+      parser: await import('jsonc-eslint-parser'),
+    },
+  },
 ]);
